fe2bf634e922921db0bd6ff5bc75c4dc6e333925
oussamaAlouat <al317257@uji.es>
2019-09-27 20:05:02 +0200

    Refactore: Extract all column code on new comonent

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,44 +1,14 @@
 <template>
   <div class="board">
     <div class="flex flex-row items-start">
-      <div
-        class="column"
+      <BoardColumn
         v-for="(column, $columnIndex) of board.columns"
         :key="$columnIndex"
-        draggable
-        @drop="moveTaskOrColumn($event, column.tasks, $columnIndex)"
-        @dragover.prevent
-        @dragenter.prevent
-        @dragstart.self="pickupColumn($event, $columnIndex)"
-        >
-        <div class="flex items-center mb-2 font-bold">
-          {{column.name}}
-        </div>
-        <div class="list-reset">
-          <div class="task"
-            v-for="(task, $taskIndex) of column.tasks"
-            :key="$taskIndex"
-            draggable
-            @dragover.prevent
-            @dragenter.prevent
-            @dragstart="pickupTask($event, $taskIndex, $columnIndex)"
-            @click="goToTask(task)"
-            @drop.stop="moveTaskOrColumn($event, column.tasks, $columnIndex ,$taskIndex)"
-            >
-            <span class="w-full flex-no-shrink font-bold">
-              {{task.name}}
-            </span>
-            <p v-if="task.description" class="w-full flex-no-shink mt-1 text-sm">
-              {{task.description}}
-            </p>
-          </div>
-        </div>
-        <input
-          type="text"
-          class="block p-2 w-full bg-transparent"
-          placeholder="Enter new task"
-          @keyup.enter="createTask($event, column.tasks)"/>
-      </div>
+        :column="column"
+        :columnIndex="$columnIndex"
+        :board="board"
+      />
+
       <div class="column flex">
         <input
           type="text"
@@ -61,7 +31,11 @@
 
 <script>
 import {mapState} from 'vuex';
+import BoardColumn from '@/components/BoardColumn';
 export default {
+  components: {
+    BoardColumn
+  },
   data() {
     return {
       newColumnName: ''
@@ -74,9 +48,6 @@ export default {
     },
   },
   methods: {
-    goToTask(task) {
-      this.$router.push({name: 'task', params: {id: task.id}});
-    },
     close() {
       this.$router.push({name: 'board'});
     },
@@ -88,54 +59,6 @@ export default {
         this.newColumnName = ''
       }
     },
-    createTask(event, tasks) {
-      console.log(event)
-      this.$store.commit('CREATE_TASK', { tasks, name: event.target.value});
-      event.target.value = '';
-    },
-    pickupTask (event, taskIndex, fromColumnIndex) {
-      event.dataTransfer.effectAllowed = 'move';
-      event.dataTransfer.dropEffect = 'move';
-
-      event.dataTransfer.setData('from-task-index', taskIndex);
-      event.dataTransfer.setData('from-column-index', fromColumnIndex);
-      event.dataTransfer.setData('type', 'task');
-
-    },
-    pickupColumn(event, fromColumnIndex) {
-      event.dataTransfer.effectAllowed = 'move';
-      event.dataTransfer.dropEffect = 'move';
-
-      event.dataTransfer.setData('from-column-index', fromColumnIndex);
-      event.dataTransfer.setData('type', 'column');
-
-    },
-    moveTaskOrColumn(event, toTasks,toColumnIndex, toTaskIndex) {
-      const type = event.dataTransfer.getData('type');
-      if(type === 'task') {
-        this.moveTask(event, toTasks, toTaskIndex !== undefined ? toTaskIndex : toTasks.length);
-      } else {
-        this.moveColumn(event, toColumnIndex);
-      }
-    },
-    moveTask(event, toTasks, toTaskIndex) {
-      const fromColumnIndex = event.dataTransfer.getData('from-column-index');
-      const fromTaskIndex = event.dataTransfer.getData('from-task-index');
-      const fromTasks = this.board.columns[fromColumnIndex].tasks;
-      this.$store.commit('MOVE_TASK', {
-        fromTasks,
-        fromTaskIndex,
-        toTasks,
-        toTaskIndex
-      });
-    },
-    moveColumn(event, toColumnIndex) {
-      const fromColumnIndex = event.dataTransfer.getData('from-column-index');
-      this.$store.commit('MOVE_COLUMN', {
-        fromColumnIndex,
-        toColumnIndex
-      });
-    }
   },
 };
 </script>
@@ -145,10 +68,6 @@ export default {
   @apply flex items-center flex-wrap shadow mb-2 py-2 px-2 rounded bg-white text-grey-darkest no-underline;
 }
 
-.column {
-  @apply bg-grey-light p-2 mr-4 text-left shadow rounded;
-  min-width: 350px;
-}
 
 .board {
   @apply p-4 bg-teal-dark h-full overflow-auto;
