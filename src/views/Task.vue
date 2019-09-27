f748e9c17a3d220288c98c4dd3f8b1303166da85
oussamaAlouat <al317257@uji.es>
2019-09-27 13:48:50 +0200

    Added description on task component

--- src/views/Task.vue
+++ src/views/Task.vue
@@ -2,6 +2,10 @@
   <div class="task-view">
     <div class="flex flex-col flex-grow items-start justify-between px-4">
       {{task.name}}
+      <textarea
+        class="relative bg-transparent px-2 mt-2 h-64 border-none leading-normal"
+        :value="task.description">
+      </textarea>
     </div>
   </div>
 </template>
