4af5919def9a2caeb341ec548ecb1f326322e805
oussamaAlouat <al317257@uji.es>
2019-09-27 13:48:30 +0200

    Added create task funtion on vuex

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import defaultBoard from './default-board'
-import { saveStatePlugin } from './utils';
+import { saveStatePlugin, uuid } from './utils'
 
 Vue.use(Vuex)
 
@@ -12,7 +12,15 @@ export default new Vuex.Store({
   state: {
     board
   },
-  mutations: {},
+  mutations: {
+    CREATE_TASK (state, { tasks, name }) {
+      tasks.push({
+        name,
+        id: uuid(),
+        description: ''
+      })
+    }
+  },
   getters: {
     getTask (state) {
       return (id) => {
