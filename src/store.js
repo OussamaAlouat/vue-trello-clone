e0ee7ce360afbfc7b89e3921e6e2aed98e59b26b
oussamaAlouat <al317257@uji.es>
2019-09-27 17:57:39 +0200

    Function to create new column in store

--- src/store.js
+++ src/store.js
@@ -20,6 +20,12 @@ export default new Vuex.Store({
         description: ''
       })
     },
+    CREATE_COLUMN (state, { name }) {
+      state.board.columns.push({
+        name,
+        tasks: []
+      });
+    },
     UPDATE_TASK (state, { task, key, value }) {
       task[key] = value;
       // Vue.set(task, key, value)
@@ -32,7 +38,7 @@ export default new Vuex.Store({
       const columnList = state.board.columns;
       const columnToMove = columnList.splice(fromColumnIndex, 1)[0];
       columnList.splice(toColumnIndex, 0, columnToMove);
-    }
+    },
   },
   getters: {
     getTask (state) {
