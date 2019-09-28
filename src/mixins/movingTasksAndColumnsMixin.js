115df3908a5119cd9250206826239823e89af5d5
oussamaAlouat <al317257@uji.es>
2019-09-28 10:42:44 +0200

    Creating new mixin file

---
+++ src/mixins/movingTasksAndColumnsMixin.js
@@ -0,0 +1,44 @@
+export default {
+  props: {
+    board: {
+      type: Object,
+      required: true
+    },
+    column: {
+      type: Object,
+      required: true
+    },
+    columnIndex: {
+      type: Number,
+      required: true
+    }
+  },
+  methods: {
+    moveTaskOrColumn (event, toTasks,toColumnIndex, toTaskIndex) {
+      const type = event.dataTransfer.getData('type');
+      if(type === 'task') {
+        this.moveTask(event, toTasks, toTaskIndex !== undefined ? toTaskIndex : toTasks.length);
+      } else {
+        this.moveColumn(event, toColumnIndex);
+      }
+    },
+    moveTask (event, toTasks, toTaskIndex) {
+      const fromColumnIndex = event.dataTransfer.getData('from-column-index');
+      const fromTaskIndex = event.dataTransfer.getData('from-task-index');
+      const fromTasks = this.board.columns[fromColumnIndex].tasks;
+      this.$store.commit('MOVE_TASK', {
+        fromTasks,
+        fromTaskIndex,
+        toTasks,
+        toTaskIndex
+      });
+    },
+    moveColumn (event, toColumnIndex) {
+      const fromColumnIndex = event.dataTransfer.getData('from-column-index');
+      this.$store.commit('MOVE_COLUMN', {
+        fromColumnIndex,
+        toColumnIndex
+      });
+    }
+  }
+}
