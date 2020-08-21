b16517fd075d235e23dadcafa90000b38d79950e
OussamaAlouat <al317257@uji.es>
2020-08-21 13:32:09 +0200

    Added BoardColumn test

---
+++ tests/unit/components/BoardColumn.spec.js
@@ -0,0 +1,45 @@
+import VueRouter from 'vue-router';
+import vuex from 'vuex';
+
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import BoardColumn from '@/components/BoardColumn.vue'
+import AppDrop from '@/components/AppDrop.vue';
+import AppDrag from '@/components/AppDrag.vue';
+import ColumnTask from '@/components/ColumnTask.vue';
+import defaultBoard from '@/default-board'
+import state from '@/store/state';
+
+const localVue = createLocalVue();
+localVue.use(vuex);
+
+localVue.use(VueRouter);
+
+describe('BoardColumn component', () => {
+  let wrapper;
+  let store;
+  beforeEach(() => {
+    store = new vuex.Store({
+      state
+    });
+
+    wrapper = shallowMount(BoardColumn,
+      {
+        store,
+        localVue,
+        components: {
+          AppDrag,
+          AppDrop,
+          ColumnTask
+        },
+        propsData: {
+          board: { ...defaultBoard },
+          column: {},
+          columnIndex: 0
+        }
+      });
+  });
+
+  it('BoardColumn is vue component', () => {
+    expect(wrapper.isVueInstance()).toBeTruthy();
+  });
+});
