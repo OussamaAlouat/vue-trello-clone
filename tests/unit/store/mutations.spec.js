63d3d55ae166c95d0e934e3af0247f40947c399d
OussamaAlouat <al317257@uji.es>
2020-08-24 10:18:12 +0200

    Added tests to verify CREATE_TASKS on undefined parameters work well

--- tests/unit/store/mutations.spec.js
+++ tests/unit/store/mutations.spec.js
@@ -17,5 +17,17 @@ describe('CREATE_TASK', () => {
       mutations.CREATE_TASK(state, { name: null, tasks });
       expect(tasks).toBe(null);
     });
+
+    it('name parameter should be undefined', () => {
+      const tasks = [];
+      mutations.CREATE_TASK(state, { name: undefined, tasks });
+      expect(tasks.length).toBe(0)
+    });
+
+    it('tasks parameter should be undefined', () => {
+      const tasks = undefined;
+      mutations.CREATE_TASK(state, { name: null, tasks });
+      expect(tasks).toBe(undefined);
+    });
   });
 });
